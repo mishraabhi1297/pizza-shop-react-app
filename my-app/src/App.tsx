44588285c99b6b5fc228b96f3116396e8d1bc8d7
Abhishek Mishra <abhishek.mishra@genebygene.com>
2023-07-27 13:59:06 +1000

    Added menu items to change view

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,50 @@
+import { useState } from "react";
+import "./App.css";
 import Order from "./components/Order";
+import { MenuItems } from "./constants/menuItemConstants";
 
 function App() {
+  const [currentMenu, setCurrentMenu] = useState(MenuItems.PlaceOrder);
+
+  const changeMenu = (item: string) => {
+    setCurrentMenu(item);
+  };
+
+  const displayMenuItem = () => {
+    switch (currentMenu) {
+      case MenuItems.AddLocation:
+        return <div></div>;
+      case MenuItems.UpdateMenu:
+        return <div></div>;
+      default:
+        return <Order />;
+    }
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Welcome to Dynamite Pizza</h1>
-      <Order />
+      <div style={{ display: "flex" }}>
+        <div
+          className="menu-item"
+          onClick={() => changeMenu(MenuItems.PlaceOrder)}
+        >
+          Place order
+        </div>
+        <div
+          className="menu-item"
+          onClick={() => changeMenu(MenuItems.AddLocation)}
+        >
+          Add new location
+        </div>
+        <div
+          className="menu-item"
+          onClick={() => changeMenu(MenuItems.UpdateMenu)}
+        >
+          Change menu
+        </div>
+      </div>
+      {displayMenuItem()}
     </div>
   );
 }
---
+++ my-app/src/constants/menuItemConstants.ts
@@ -0,0 +1,5 @@
+export const MenuItems = {
+  PlaceOrder: "place_order",
+  AddLocation: "add_location",
+  UpdateMenu: "update_menu"
+}
